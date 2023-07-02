16d42a4b2ef5d968f7c7d11b43ab27347d3e1866
lohit244 <lohit244@gmail.com>
2023-07-02 18:02:59 +0530

    trying to fix broken vercel build?

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -4,9 +4,8 @@ import "@/styles/globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import type { AppProps } from "next/app";
 
-const queryClient = new QueryClient();
-
 export default function App({ Component, pageProps }: AppProps) {
+  const queryClient = new QueryClient();
   return (
     <>
       <QueryClientProvider client={queryClient} >
